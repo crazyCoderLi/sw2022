ac6a2d4c98377d95c195abc9a9738c0ca8147b52
crazyCoderLi <lyq17616795250@163.com>
2022-04-08 08:45:56 +0800

    change layout style

--- src/MainLayout/MainLayout.js
+++ src/MainLayout/MainLayout.js
@@ -13,27 +13,31 @@ const MainLayout = () => {
 
     return (
         <Layout>
-            <Header className="header">
-                <div className="logo"/>
+            <Header className="header" style={{height: "10vh", padding: "10px 20px"}}>
+                <h1 style={{
+                    fontSize: "30px",
+                    color: "white"
+                }}>餐盘自助结算系统管理员端</h1>
+                {/*<div className="logo"/>*/}
                 {/*<Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>*/}
                 {/*    <Menu.Item key="1">nav 1</Menu.Item>*/}
                 {/*    <Menu.Item key="2">nav 2</Menu.Item>*/}
                 {/*    <Menu.Item key="3">nav 3</Menu.Item>*/}
                 {/*</Menu>*/}
             </Header>
-            <Content style={{padding: '0 50px', height: "100%"}}>
+            <Content style={{padding: '0 50px', height: "80vh"}}>
                 {/*<Breadcrumb style={{margin: '16px 0'}}>*/}
                 {/*    <Breadcrumb.Item>Home</Breadcrumb.Item>*/}
                 {/*    <Breadcrumb.Item>List</Breadcrumb.Item>*/}
                 {/*    <Breadcrumb.Item>App</Breadcrumb.Item>*/}
                 {/*</Breadcrumb>*/}
                 <Layout className="site-layout-background" style={{padding: '24px 0'}}>
-                    <Sider className="site-layout-background" width={200}>
+                    <Sider className="site-layout-background" width={300} style={{height: "50vh"}}>
                         <Menu
                             mode="inline"
                             defaultSelectedKeys={["ModelManagementPage"]}
                             // defaultOpenKeys={['sub1']}
-                            style={{height: '100%'}}
+                            style={{height: '100%', fontSize: "20px",}}
                             onSelect={
                                 (item) => {
                                     console.log(item);
@@ -66,8 +70,8 @@ const MainLayout = () => {
                                 <Menu.Item key="AddUser">增加人员信息</Menu.Item>
                                 <Menu.Item key="UserQuery">查询人员信息</Menu.Item>
                             </SubMenu>
-                            <SubMenu key="DatasetManagement" title="**管理">
-                                <Menu.Item key="DatasetManagementPage">**</Menu.Item>
+                            <SubMenu key="DatasetManagement" title="充值管理">
+                                <Menu.Item key="DatasetManagementPage">充值管理</Menu.Item>
                             </SubMenu>
                             <SubMenu key="ConsumeRecordManagement" title="明细管理">
                                 <Menu.Item key="ConsumeRecordQuery">明细查询</Menu.Item>
@@ -77,7 +81,7 @@ const MainLayout = () => {
                     <Content style={{padding: '0 24px', minHeight: 280}}><Outlet></Outlet></Content>
                 </Layout>
             </Content>
-            <Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Footer>
+            <Footer style={{textAlign: 'center', height: "10vh"}}>Ant Design ©2018 Created by Ant UED</Footer>
         </Layout>
     )
 }
